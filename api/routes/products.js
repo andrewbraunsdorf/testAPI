0244fe9ecd9bff49d2a8cc457a543bd63d0f6398
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-03-08 00:42:17 +0000

    added get and delete for orders, have a basic API

--- api/routes/products.js
+++ api/routes/products.js
@@ -8,7 +8,7 @@ router.get("/", (req, res, next) => {
 });
 
 router.post("/", (req, res, next) => {
-	res.status(200).json({
+	res.status(201).json({
 		message: "Handling POST requests to /products"
 	});
 });
@@ -25,5 +25,17 @@ router.get("/:productId", (req, res, next) => {
 			message: "You passed an ID"
 		});
 	}
+});
+
+router.patch("/:productId", (req, res, next) => {
+	res.status(200).json({
+		message: "Updated Product!"
+	});
+});
+
+router.delete("/:productId", (req, res, next) => {
+	res.status(200).json({
+		message: "Deleted Product!"
+	});
 });
 module.exports = router;
