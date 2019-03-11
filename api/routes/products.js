046da008b0c5f62065074eb18e638acfd74965c1
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-03-11 22:32:09 +0000

    added url and types

--- api/routes/products.js
+++ api/routes/products.js
@@ -5,10 +5,23 @@ const mongoose = require('mongoose');
 const Product = require("../models/product");
 
 router.get("/", (req, res, next) => {
-	Product.find().exec().then(docs => {
-		console.log(docs);
+	Product.find().select('name price _id').exec().then(docs => {
+		const response = {
+			count: docs.length,
+			products: docs.map(doc => {
+				return {
+					name: doc.name,
+					price: doc.price,
+					_id: doc._id,
+					request: {
+						type: 'GET',
+						url: 'https://andrewbraunsdorf2018-andrewbraunsdorf.c9users.io/products/' + doc._id
+					}
+				};
+			})
+		};
 		// if (docs.length >= 0) {
-			res.status(200).json(docs);
+			res.status(200).json(response);
 		// } else {
 		// 	res.status(404).json(docs);
 		// }
